395a866907fb2565de380d5412833884939829ea
jaffarabbas <gamaportal8@gmail.com>
2020-03-29 16:18:16 +0500

    .

--- task4.java
+++ task4.java
@@ -1,23 +1,34 @@
-/**
- * task4
- */
 
-
-
-
-
+import java.util.Scanner;
 
 class Account
-{
-    
+{ 
+    static Scanner obj = new Scanner(System.in);
+    static String accountTitle;
+    static Long AccountNumber;
+    static Long LastNumber;
+
+    static void ac()
+    {
+        long Accountseries=100000000000000L + obj.nextLong();
+        AccountNumber = Accountseries;
+        System.out.println(AccountNumber);
+        LastNumber=AccountNumber;
+    }
 }
 
 
 
 
- public class task4 {
+ class task4 {
 
     public static void main(String[] args) {
-        
+     Scanner obj = new Scanner(System.in);
+     char a = obj.next().charAt(0);
+     if(a =='a'){
+        Account.show();
+     }else if(a == 'b'){
+        System.out.println(Account.LastNumber);
+     }
     }
 }
